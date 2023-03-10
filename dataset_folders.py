8ca456e0f5ca3578c5c92bf17fbea1203d9152dd
rikner <rikner@web.de>
2023-03-10 17:36:40 +0100

    improve feature extraction

--- dataset_folders.py
+++ dataset_folders.py
@@ -4,14 +4,9 @@ import os
 
 dataset_path = "./dataset/"
 spectrogram_path = join(dataset_path, "spectrograms3sec")
-# spectrogram_path_train = join(spectrogramt_path, "train")
-# spectrogram_path_test = join(spectrogramt_path, "test")
-audio_path = join(dataset_path, "audio3sec")
 
-def main():
+def create_dataset_folders():
     os.makedirs(spectrogram_path)
-    # os.makedirs(spectrogram_path_train)
-    # os.makedirs(spectrogram_path_test)
 
     audio_files = glob("./giantsteps-key-dataset/audio/*.wav")
 
@@ -33,17 +28,8 @@ def main():
         ids_for_genre = [id_g[0] for id_g in id_genres if id_g[1] == genre]
         genre_counts.append((genre, len(ids_for_genre)))
         print(f'{genre}: {len(ids_for_genre)}')
-
-        path_audio = os.path.join(audio_path, f'{genre}')
-        os.makedirs(path_audio)
-
         os.makedirs(os.path.join(spectrogram_path, f'{genre}'))
-
-        # path_train = os.path.join(spectrogram_path_train, f'{genre}')
-        # path_test = os.path.join(spectrogram_path_test, f'{genre}')
-        # os.makedirs(path_train)
-        # os.makedirs(path_test)
 
 
 if __name__ == "__main__":
-    main()
+    create_dataset_folders()
